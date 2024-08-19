d6fb2e2e91ad3515c6fdd741c64867db714579eb
LucasSantos <lucas.fariasbsi@hotmail.com>
2024-08-19 00:34:16 -0300

    first commit

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,19 @@
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+
+	}
+
+	@Quando("^executa-lo$")
+	public void executa_lo() throws Throwable {
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	}
+}
