f3103f822d8e4d47a5a9a016fb109dc72b3c2b90
LucasSantos <lucas.fariasbsi@hotmail.com>
2024-08-20 01:55:28 -0300

    teste 01

--- src/test/java/steps/LogarContaSteps.java
+++ src/test/java/steps/LogarContaSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,7 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
-import junit.framework.Assert;
+
+
 
 public class LogarContaSteps {
 	
